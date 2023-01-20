96200412bc52f0c1a04747f9c59e1911137030ab
Sudarshan Singh <jhengis@protonmail.com>
2023-01-20 20:01:35 +0530

    Email splicer program

---
+++ Email_Slicer.py
@@ -0,0 +1,19 @@
+# Collect email form user
+# Splict the emai using the @
+# Save first part as the user name and seconf part as domain
+# split domain using .
+
+def main():
+  print("Welcome to the email slicer ")
+  print('')
+
+  email_input = input("Enter your email address: ")
+
+  (username, domain) = email_input.split("@")
+  (domain, extension) = domain.split(".")
+
+  print("Username : ", username)
+  print("Domain : ", domain)
+  print("Extension: ", extension)
+
+main()
